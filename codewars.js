e8d89c9c20f00dc4a40247a2ddb01a7b750b39a4
Kasiei <k25ts09@outlook.jp>
2025-03-19 14:19:26 +0900

    managed hover action !

--- codewars.js
+++ codewars.js
@@ -9,3 +9,17 @@ window.addEventListener('scroll', ()=> {
     boxs[0].style.transform = `translateY(-${window.scrollY * 1}px)`;
     boxs[1].style.transform = `translateY(-${window.scrollY * 0.4}px)`;
 })
+
+const list = document.querySelectorAll('.header__list');
+const links = document.querySelectorAll('.header__link');
+const linksSpan = document.querySelectorAll('.header__link--sub');
+for (let index = 0; index < links.length; index++) {    
+    links[index].addEventListener('mouseover', () => {
+        linksSpan[index].classList.add('active');
+        links[index].classList.add('active');
+    });
+
+    links[index].addEventListener('mouseleave', () => {
+        linksSpan[index].classList.remove('active');
+    });
+}
