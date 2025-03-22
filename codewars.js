58821bc7480ff23b393a21b800a3ed73be245446
Kasiei <k25ts09@outlook.jp>
2025-03-22 15:13:32 +0900

    study BEM code commis

--- codewars.js
+++ codewars.js
@@ -1,14 +1,14 @@
 
 //parallex effect scroll
 
-const boxs = document.querySelectorAll('.box');
+// const boxs = document.querySelectorAll('.box');
 
-window.addEventListener('scroll', ()=> {
-    console.log(window.scrollY);
+// window.addEventListener('scroll', ()=> {
+//     console.log(window.scrollY);
     
-    boxs[0].style.transform = `translateY(-${window.scrollY * 1}px)`;
-    boxs[1].style.transform = `translateY(-${window.scrollY * 0.4}px)`;
-})
+//     boxs[0].style.transform = `translateY(-${window.scrollY * 1}px)`;
+//     boxs[1].style.transform = `translateY(-${window.scrollY * 0.4}px)`;
+// })
 
 const list = document.querySelectorAll('.header__list');
 const links = document.querySelectorAll('.header__link');
