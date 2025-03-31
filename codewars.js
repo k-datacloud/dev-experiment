d4a55b22756c9b955d62d7179aa3be7ad1be4783
Kasiei <k25ts09@outlook.jp>
2025-03-31 20:59:23 +0900

    code commits

--- codewars.js
+++ codewars.js
@@ -65,11 +65,15 @@ window.addEventListener('scroll', () => {
     
 });
 
+
+
+//language switch
 const langSwitch = document.querySelectorAll('.c-lang-toggle');
 const introTitles = document.querySelectorAll('.intro__title');
 const introTexts = document.querySelectorAll('.intro__text');
 const showcaseTitles = document.querySelectorAll('.p-single-showcase__title--small');
 const showcaseTexts = document.querySelectorAll('.p-single-showcase__text');
+const showcaseListItems = document.querySelectorAll('.p-single-showcase-list__item');
 for (let index = 0; index < langSwitch.length; index++) {
     langSwitch[index].addEventListener('click', () => {
 
@@ -92,6 +96,10 @@ for (let index = 0; index < langSwitch.length; index++) {
                 text.textContent = text.getAttribute('data-jp');
                 text.classList.add('p-single-showcase__text--jp');
             })
+            showcaseListItems.forEach((item) => {
+                item.textContent = item.getAttribute('data-jp');
+                item.classList.add('p-single-showcase-list__item--jp');
+            })
         } else {
             langSwitch[index].classList.add('active-lang');
             langSwitch[index + 1].classList.remove('active-lang'); 
@@ -107,7 +115,35 @@ for (let index = 0; index < langSwitch.length; index++) {
                 text.textContent = text.getAttribute('data-en');
                 text.classList.remove('p-single-showcase__text--jp');
             })
+            showcaseTitles.forEach((title) => {
+                title.textContent = title.getAttribute('data-en');
+                title.classList.remove('p-single-showcase__title--jp');
+            })
+            showcaseListItems.forEach((item) => {
+                item.textContent = item.getAttribute('data-en');
+                item.classList.remove('p-single-showcase-list__item--jp');
+            })
         }
 
     });
 }
+
+
+// menuBtn
+const menuBtn = document.querySelector('.menu-btn');
+const headerNav = document.querySelector('.header__nav');
+
+menuBtn.addEventListener('click', () => {
+    
+    if (headerNav.classList.contains('menu-close')) {
+        headerNav.classList.remove('menu-close');
+        headerNav.classList.add('menu-active');
+        menuBtn.textContent = menuBtn.getAttribute('data-close');
+        menuBtn.classList.add('menu-btn--close');
+    } else {
+        headerNav.classList.remove('menu-active');
+        headerNav.classList.add('menu-close');
+        menuBtn.textContent = menuBtn.getAttribute('data-open');
+        menuBtn.classList.remove('menu-btn--close');
+    }
+})
