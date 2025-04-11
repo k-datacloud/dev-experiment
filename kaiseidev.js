cc8b0404155175d030560eda29468b8363a3509e
Kasiei <k25ts09@outlook.jp>
2025-04-11 20:54:26 +0900

    code commits

--- kaiseidev.js
+++ kaiseidev.js
@@ -1,4 +1,5 @@
 const heroTitle = document.querySelectorAll('.hero__title-letter');
+const dummyText = document.querySelector('.dummy p');
 
 heroTitle.forEach ((title) => {
     title.addEventListener('animationend', () => {
@@ -10,8 +11,12 @@ window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
     const speed = 0.2; // Adjust the speed to your liking
     const ypos = scrollY * speed;
+    console.log(ypos);
+    
     
     heroTitle.forEach ((title) => {
         title.style.transform = `translateY(${ypos}px)`;
     })
+
+    dummyText.style.fontSize = `calc(${ypos}/1440 * 100vw)`;
 })
